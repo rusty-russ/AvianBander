d68f291647cc66fe52459ea8494671c989e521f5
Austin Russell <aruss37@gmail.com>
2026-07-02 09:42:29 -0400

    Initial Commit

---
+++ AvianBander/AvianBanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvianBanderApp.swift
+//  AvianBander
+//
+//  Created by Austin Russell on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AvianBanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvianBanderTests/AvianBanderTests.swift
@@ -0,0 +1,19 @@
+//
+//  AvianBanderTests.swift
+//  AvianBanderTests
+//
+//  Created by Austin Russell on 7/2/26.
+//
+
+import Testing
+@testable import AvianBander
+
+struct AvianBanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
